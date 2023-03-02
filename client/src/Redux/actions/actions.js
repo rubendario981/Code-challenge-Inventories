0f49bb6cf326105b0da5f67099bbb6b2facc5ed8
Ruben Guzman <rubendario981@hotmail.com>
2023-03-02 17:58:53 -0500

    Create actions to fetch data, create new record and logout

--- client/src/Redux/actions/actions.js
+++ client/src/Redux/actions/actions.js
@@ -1,29 +1,99 @@
 import axios from "axios";
 const URL = "http://localhost:3000";
 
-export function getUser(id) {
+export function login(data) {
 	return async (dispatch) => {
-		const response = await axios.get(`${URL}/user/${id}`);
-		return dispatch({
-			type: "GET_USER",
-			payload: response.data,
-		});
+		try {
+			const response = await axios.post(`${URL}/user/login`, data);			
+			const token = JSON.stringify(response.data)
+			localStorage.setItem("token", token)
+			dispatch({
+				type: "LOGIN",
+				payload: JSON.parse(window.atob(token?.split('.')[1]))
+			});
+			return response
+		} catch (error) {
+			console.log(error.response);
+			return error.response
+		}
 	};
 }
 
 export function createUser(data) {
 	return async (dispatch) => {
 		try {
-			const response = await axios.post(`${URL}/user`, data);
+			const response = await axios.post(`${URL}/user/signup`, data);
+			const token = JSON.stringify(response.data)
+			localStorage.setItem("token", token)
 			dispatch({
 				type: "CREATE_USER",
-				payload: response.data,
+				payload: JSON.parse(window.atob(token?.split('.')[1]))
 			});
-			console.log(response);
 			return response
 		} catch (error) {
 			console.error(error.response);
 			return error.response
 		}
 	};
+}
+
+export function identifyUser() {
+	return async (dispatch) => {
+		try {			
+			const token = localStorage.getItem("token")
+			dispatch({
+				type: "IDENTIFY_USER",
+				payload: JSON.parse(window.atob(token?.split('.')[1]))
+			});
+		} catch (error) {
+			console.error(error);
+			return error.response
+		}
+	};
+}
+
+export function logout() {
+	return async (dispatch) => {
+		try {			
+			localStorage.setItem("token", "")
+			dispatch({
+				type: "LOGOUT"
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	};
+}
+
+export function createActive(data) {
+	return async (dispatch) => {
+		try {
+			const response = await axios.post(`${URL}/product/new`, data);
+			dispatch({
+				type: "CREATE_ACTIVE"
+			});
+			return response
+		} catch (error) {
+			console.error("error ",error.response);
+			return error.response
+		}
+	};
+}
+
+export function getRecordsById(id) {
+	return async (dispatch) => {
+		try {
+			const response = await axios.get(`${URL}/product/user-record/${id}`);
+			dispatch({
+				type: "GET_LIST_ACTIVES_BY_USER",
+				payload: response.data
+			});
+			return response
+			
+		} catch (error) {
+			console.error("error ",error.response);
+			return error.response
+
+		}
+	}
 }
--- client/src/Redux/reducer/reducer.js
+++ client/src/Redux/reducer/reducer.js
@@ -1,21 +1,45 @@
 const initialState = {
-  user: {}
+  user: {},
+  listActivesUser: []
+
 }
+
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "GET_USER":
+    case "LOGIN":
       return {
         ...state,
         user: action.payload,
       };
+
     case "CREATE_USER":
       return {
         ...state,
         user: action.payload,
       };
+    case "LOGOUT":
+      return {
+        ...state,
+        user: {},
+      };
+
+    case "IDENTIFY_USER":
+      return {
+        ...state,
+        user: action.payload,
+      };
+
+    case "CREATE_ACTIVE":
+      return { ...state };
+
+    case "GET_LIST_ACTIVES_BY_USER":
+      return {
+        ...state,
+        listActivesUser: action.payload
+      };
 
 
-      
+
     default:
       return { ...state };
   }
